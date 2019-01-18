2ba870e25882c815c9da7b97aa0608e28a9fb380
Slater Smith <smithslater@gmail.com>
2019-01-18 12:10:04 -0800

    set variables for form questions one through four and create hide and show javascript commands for second form questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-
-
-    var intro = parseInt($("select#intro").val());
+    var intro = $("select#intro-class").val();
+    var track = $("select#front-back-end").val();
+    var size = $("select#company-size").val();
+    var build = $("select#building").val();
+    var interest = $("select#interested").val();
 
     console.log(intro);
+    console.log(track);
+    console.log(size);
+    console.log(build);
+    console.log(interest);
 
-    if (intro === 1){
+
+    if (intro === "No"){
       $("#track-intro").show();
+    } else {
+      $("#track-intro").hide();
+    }
+    if (track === "Front-end all the way!"){
+      $("#track-webdev").show();
+    } else if (track === "How about a combination!") {
+      $("#track-ruby").show();
     }
 
 
